61ad09305a75d984e52c15ea40d8c4942e4907b3
Devan <deshlock010101@hotmai.com>
2022-02-11 13:49:30 -0600

    completed the assignment

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -10,8 +10,12 @@ namespace MethodExercise
             string color;
             string animal;
             string band;
+            int num1 = 4;
+            int num2 = 7;
+            
+            
 
-
+            
             Console.Write("What is your name?: ");
             name = Console.ReadLine();
             Console.Write($"Hi {name} what is your favorite color?: ");
@@ -20,9 +24,40 @@ namespace MethodExercise
             animal = Console.ReadLine();
             Console.Write("What is your favorite band?: ");
             band = Console.ReadLine();
+            
+            Console.WriteLine($"My name is {name}, my favorite color is {color}, my favorite animal is a {animal}, and my favorite band is {band}. ");
+
+            Sum(6,4);
+            Subtract(4, 3);
+            Multiply(12, 3);
+            Divide(36, 12);
+            
+            
+        }
 
 
-            Console.Write($"My name is {name}, my favorite color is {color}, my favorite animal is {animal}, and my favorite band is {band}. ");
+        public static int Sum(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            Console.WriteLine(num1 / num2);
+            return num1 / num2;
         }
     }
 }
