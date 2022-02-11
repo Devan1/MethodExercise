1b588925e58c2879f1d3736f25b4ade75929edce
Devan <deshlock010101@hotmai.com>
2022-02-11 11:22:33 -0600

    user story

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -6,7 +6,23 @@ namespace MethodExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            string color;
+            string animal;
+            string band;
+
+
+            Console.Write("What is your name?: ");
+            name = Console.ReadLine();
+            Console.Write($"Hi {name} what is your favorite color?: ");
+            color = Console.ReadLine();
+            Console.Write("What is your favorite animal?: ");
+            animal = Console.ReadLine();
+            Console.Write("What is your favorite band?: ");
+            band = Console.ReadLine();
+
+
+            Console.Write($"My name is {name}, my favorite color is {color}, my favorite animal is {animal}, and my favorite band is {band}. ");
         }
     }
 }
